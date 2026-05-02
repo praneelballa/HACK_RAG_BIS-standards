ad99c35fd6c759f849702fdf8eb12d2f5fe8bdfd
praneel <praneel.balla.dev#gmail.com>
2026-05-03 00:33:03 +0530

    Final submission - BIS RAG Hackathon

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,4 +1,4 @@
-from qdrant_client import QdrantClient
+    from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams, PointStruct
 from sentence_transformers import SentenceTransformer
 import json
